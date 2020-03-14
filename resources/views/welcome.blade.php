25ba1286ca75ee244bb87afda59ee91d13be4e80
hiitstya <muttyakhasanah@gmail.com>
2020-03-14 09:34:49 +0700

    merubah tampilan welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Workshop
+                    tya
                 </div>
 
                 <div class="links">
